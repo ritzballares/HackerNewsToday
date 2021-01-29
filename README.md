81b95b85f386a18565fe7c304a1a45694f18a65d
Ritz Ballares <ritzballares@gmail.com>
2021-01-29 18:44:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hacker News Today
+Hacker News Today is a chrome extension that displays the top stories from [Hacker News](https://news.ycombinator.com/).
+
+TODO:
+- Prettify UI
+- Publish chrome extension
+- Fix bug: Duplicated stories if user decides to click on *Update list* twice (or more)
---
+++ popup.js
@@ -0,0 +1,29 @@
+let updateListButton = document.getElementById("updateList");
+
+updateListButton.addEventListener("click", () => {
+    fetchData();
+});
+
+function fetchData() {
+    fetch("http://hn.algolia.com/api/v1/search?tags=front_page")
+        .then(response => response.json())
+        .then(data => {    
+            // If a has more points, a gets assigned a lower index value
+            let sortedStoriesList = data.hits.sort((a,b) => (a.points > b.points ? -1 : 1));
+
+            let numStories = document.getElementById("numberOfStories");
+            let storiesList = document.getElementById("storiesList");
+
+            for (i = 0; i < numStories.value; i++) { 
+                let storyLink = document.createElement("a");
+                storyLink.href = sortedStoriesList[i].url;
+                storyLink.text = sortedStoriesList[i].title;
+                storyLink.target = "_blank";
+
+                let storiesListItem = document.createElement("li");
+                storiesListItem.appendChild(storyLink);
+
+                storiesList.appendChild(storiesListItem);
+            }
+        });
+}
